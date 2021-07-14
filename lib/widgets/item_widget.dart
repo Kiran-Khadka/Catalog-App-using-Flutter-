7bd1f5634efaa4153d156e85ead5b9fb26143926
Kiran Khadka <khadka.kiran1110@gmail.com>
2021-07-14 22:29:51 +0545

    Day 12 Completed Item Widget on adding items

---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+import 'package:second_app/models/catalog.dart';
+
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item}) 
+  : assert(item!=null),
+  super(key: key);
+  
+ //* assert is used only in debugging process to check null safety 
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+       shape: StadiumBorder(),
+          child: ListTile(
+             onTap: () {
+               print("${item.name} press"); // $ is for multiple holding
+             },
+            leading:Image.network(item.image),
+            title: Text(item.name),
+            subtitle: Text(item.desc),
+            //trailing: Text (item.price.toString()),
+            trailing: Text("\$${item.price}",
+            textScaleFactor: 1.4,
+            style: TextStyle(
+              color: Colors.deepOrange,
+              fontWeight: FontWeight.bold,
+            ),), // two $ is for adding the variable
+            
+         ),
+    );
+  }
+}
